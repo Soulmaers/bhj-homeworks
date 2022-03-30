a8c55ad555ab0315db963544e4d2d98f197a8fc9
Soulmaers <certiss@ya.ru>
2022-03-30 18:41:44 +0300

    Готово

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,22 +1,25 @@
 
 
-const subMenu = document.querySelectorAll('.menu_sub');//нашел все подменю которые открываются и закрываются
-
+const subMenu = document.querySelectorAll('.menu_sub');
 
 const arr = Array.from(subMenu);
 arr.forEach(element => {
-    elem = element.closest('.menu__item');//нашел внешний элемент списка
-    subLink = elem.firstElementChild; //внутри элемента списка нашел ссылку
-    subLink.onclick = function () { //добавляем обработчик события только для ссылок у которых есть подменю
-        menuLink = document.querySelector('.menu__link'); //внутри обработчика ссылка по которой клик
-        if (this.nextElementSibling.classList.contains('menu_sub')) {// если подменю которое находится рядом содержит класс ('menu_sub')
-            menuActive = this.nextElementSibling.classList.contains('menu_active');// то ищем открытое подменю
-        } if (!menuActive) {// если открытого подменю нет
-            this.nextElementSibling.classList.add('menu_active');//то добавляем открытое подменю
-        } if (menuActive) { //если активное меню есть
-            this.nextElementSibling.classList.remove('menu_active');//то удаляем открытое подменю
+    elem = element.closest('.menu__item');
+    subLink = elem.firstElementChild;
+    subLink.onclick = function () {
+
+        if (element.classList.contains('menu_active')) {
+            arr.forEach(element => element.classList.remove('menu_active'));
+            return false;
         }
+
+        arr.forEach(element => element.classList.remove('menu_active'));
+
+        if (element.classList.contains('menu_sub')) {
+            element.classList.toggle('menu_active');
+        }
+
+        return false;
     }
-    return false;
-})
+});
 
