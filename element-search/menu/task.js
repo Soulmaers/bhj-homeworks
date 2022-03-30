6e739a9c090f8d6e2f60a9da393fe9769c7e2c2d
Soulmaers <certiss@ya.ru>
2022-03-30 08:19:33 +0300

    Готово

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,19 @@
 let menuLink = document.querySelectorAll('.menu__link');
-const arrMenulink = Array.from(menuLink);
-for (let i = 0; i <= arrMenulink.length; i++) {
-    arrMenulink[i].onclick = function () {
-        const parent = arrMenulink[i].parentElement;
-        const menuUp = parent.querySelector('.menu_sub');
-        if (menuUp.className === 'menu menu_sub') {
-            menuUp.classList.add('menu_active');
-        } else {
-            menuUp.classList.add('menu_sub');
+let menuActive = document.querySelector('.menu_active');
+for (let i of menuLink) {
+    i.onclick = function () {
+        if (menuActive !== null) {
+            menuActive.classList.toggle('menu_active');
+            if (this.nextElementSibling)
+                return false;
         }
-        if (arrMenulink[i].closest('.menu_main')) {
-            return false
+        if (menuActive !== this.nextElementSibling) {
+            this.nextElementSibling.classList.toggle('menu_active');
+            return false;
         }
     }
 }
+
+
+
+
