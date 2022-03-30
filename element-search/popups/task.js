7dc469a1e0f25cfaeb515d806f87a9b40fbc915e
Soulmaers <certiss@ya.ru>
2022-03-30 07:30:09 +0300

    Готово

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,16 +1,19 @@
+
 const modalMain = document.getElementById('modal_main');
-const closed = document.getElementsByClassName('modal__close');
+const closed = document.querySelectorAll('.modal__close');
 modalMain.classList.add('modal_active');
-closed[0].onclick = () => {
-    modalMain.classList.remove('modal_active');
+
+for (let i of closed) {
+    i.onclick = function () {
+        modal = this.closest('.modal');
+        modal.classList.remove('modal_active');
+    }
 }
 const modalSuccess = document.getElementById('modal_success');
-const showSuccess = document.getElementsByClassName('show-success');
-showSuccess[0].onclick = () => {
-    modalMain.classList.remove('modal_active');
+const showSuccess = modalMain.querySelector('.show-success');
+
+showSuccess.onclick = function () {
+    modal = this.closest('.modal');
+    modal.classList.remove('modal_active');
     modalSuccess.classList.add('modal_active');
 }
-closed[2].onclick = () => {
-    modalSuccess.classList.remove('modal_active');
-}
-
