bfc2b1d6a1ae9488b2d2213e18cd879ff72a24d3
Soulmaers <certiss@ya.ru>
2022-04-20 08:22:48 +0300

    готово

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,68 +1,26 @@
 
 const taskInput = document.getElementById('task__input');
-taskInput.value = '';
-const taskList = document.querySelector('.tasks__list');
-const tasksAdd = document.querySelector('.tasks__add');
+const taskAdd = document.getElementById('tasks__add');
+const taskList = document.getElementById('tasks__list');
 
-tasksAdd.addEventListener('click', func);
+taskAdd.addEventListener('click', () => {
+    if (taskInput.value.length > 0) {
+        const container = document.createElement('div');
+        container.classList.add('task');
 
-
-
-
-function func() {
-
-    if (taskInput.value === 0) {
+        container.innerHTML = `
+      <div class="task__title">
+        ${taskInput.value}
+      </div>
+      <a href="#" class="task__remove">&times;</a>
+    `;
+        taskList.appendChild(container);
+        const removeLink = container.querySelector('.task__remove');
+        removeLink.addEventListener('click', (el) => {
+            el.target.closest('.task').remove()
+        });
+        taskInput.value = '';
+    } else {
         return false
     }
-    let container = document.createElement('div');
-    container.classList.add('task');
-    let discription = document.createElement('div');
-    discription.classList.add('task__title');
-    discription.innerHTML = taskInput.value;
-
-    let href = document.createElement('a');
-    container.appendChild(discription);
-    container.appendChild(href);
-    taskList.appendChild(container);
-    href.outerHTML = '<a href="#" class="task__remove">&times;</a>';
-    taskInput.value = '';
-    href.onclick = removeFunc;
-
-    console.log(href);
-
-}
-
-function removeFunc(event) {
-    event.parentNode.remove();
-}
-
-
-
-/*let removerOnClick = function (event) {
-    event.parentNode.remove();
-};
-
-const task = document.querySelector('#task__input');
-task.value = '';
-document.querySelector('#tasks__add').onclick = function () {
-
-    if (task.value.length == 0) {
-        return false;
-    }
-    let container = document.createElement('div');
-    container.classList.add('task');
-
-    let discription = document.createElement('div');
-    discription.classList.add('task__title');
-    discription.innerHTML = task.value;
-
-    let remover = document.createElement('a');
-    container.appendChild(discription);
-    container.appendChild(remover);
-
-    document.querySelector('#tasks__list').appendChild(container);
-    remover.outerHTML = '<a onclick="removerOnClick(this)" href="#" class="task__remove">&times;</a>';
-    task.value = '';
-
-    return false;
-};*/
+});
