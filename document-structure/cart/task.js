a5f90d936533d5ee67aa66d9324b4bfd90458396
Soulmaers <certiss@ya.ru>
2022-04-20 17:46:31 +0300

    готово

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,5 +1,5 @@
 const product = document.querySelectorAll('.product');
-
+const cart = document.querySelector('.cart__products');
 
 const productQualDec = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
 const productQualInc = Array.from(document.querySelectorAll('.product__quantity-control_inc'));
@@ -7,69 +7,39 @@ const productValue = Array.from(document.querySelectorAll('.product__quantity-va
 const buttonAdd = Array.from(document.querySelectorAll('.product__add'));
 
 productQualDec.forEach(el => {
-    el.addEventListener('click', () => {
-        let index = productQualDec.indexOf(el);
-        if (productValue[index].textContent > 1) {
-            productValue[index].textContent--;
-        }
-    })
-
+  el.addEventListener('click', () => {
+    let index = productQualDec.indexOf(el);
+    if (productValue[index].textContent > 1) {
+      productValue[index].textContent--;
+    }
+  })
 })
 productQualInc.forEach(el => {
-    el.addEventListener('click', () => {
-        let index = productQualInc.indexOf(el);
-        productValue[index].textContent++
-
-    })
-
+  el.addEventListener('click', () => {
+    let index = productQualInc.indexOf(el);
+    productValue[index].textContent++
+  })
 })
+buttonAdd.forEach(el => {
+  el.addEventListener('click', () => {
+    let index = buttonAdd.indexOf(el);
+    let val = productValue[index].textContent
+    let dataId = product[index].dataset.id;
+    let image = product[index].children[1].getAttribute("src");
+    let cartProduct = Array.from(cart.children);
 
-/*// корзина и товары
-const cart = document.querySelector(".cart__products");
-const products = [...document.querySelectorAll(".product")];
-// управление количеством товара
-const decs = [...document.querySelectorAll(".product__quantity-control_dec")];
-const incs = [...document.querySelectorAll(".product__quantity-control_inc")];
-const values = [...document.querySelectorAll(".product__quantity-value")];
-const addButtons = [...document.querySelectorAll(".product__add")];
-
-// уменьшить количество товара
-for (let dec of decs) {
-  dec.addEventListener("click", () => {
-    let index = decs.indexOf(dec);
-    if (values[index].textContent > 1) {
-      values[index].textContent--;
-    }
-  });
-}
-
-// увеличить количество товара
-for (let inc of incs) {
-  inc.addEventListener("click", () => {
-    let index = incs.indexOf(inc);
-    values[index].textContent++;
-  });
-}
-
-// добавить товар в корзину
-for (let button of addButtons) {
-  button.addEventListener("click", () => {
-    let index = addButtons.indexOf(button);
-    let value = values[index].textContent;
-    const dataId = products[index].dataset.id;
-    const imageSrc = products[index].children[1].getAttribute("src");
-    const cartProducts = [...cart.children];
-
-    if (!cartProducts.find((item) => item.dataset.id === dataId)) {
+    if (!cartProduct.find((elem) => elem.dataset.id === dataId)) {
       cart.insertAdjacentHTML(
         "afterBegin",
-        `<div class="cart__product" data-id=${dataId}><img class="cart__product-image" src=${imageSrc}><div class="cart__product-count">${value}</div></div>`
+        `<div class="cart__product" data-id=${dataId}>
+        <img class="cart__product-image" src=${image}>
+        <div class="cart__product-count">${val}</div></div>`
       );
     } else {
-      const product = cartProducts.find((item) => item.dataset.id === dataId);
-      let productCount = product.querySelector(".cart__product-count");
-      let newCount = Number(productCount.textContent) + Number(value);
-      productCount.innerText = newCount;
+      const products = cartProduct.find((item) => item.dataset.id === dataId);
+      let count = products.querySelector('.cart__product-count');
+      let newCount = Number(count.textContent) + Number(val);
+      count.innerText = newCount;
     }
-  });
-}*/
+  })
+})
